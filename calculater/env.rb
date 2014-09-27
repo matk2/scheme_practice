6f70ea6bfa3e1487539a5638727e7eaf17d7e327
Keisuke Matsubara <k.matsu9@gmail.com>
2014-09-27 11:43:57 +0900

    リスト操作

--- calculater/env.rb
+++ calculater/env.rb
@@ -29,6 +29,15 @@ module Calculater
       :false => false
     }
 
-    $global_env = [$primitive_fun_env, $boolean_env]
+    $list_env = {
+      :nil   => [],
+      :null? => [:prim, lambda{ |list| null?(list) }],
+      :cons  => [:prim, lambda{ |a, b| cons(a, b) }],
+      :car   => [:prim, lambda{ |list| car(list) }],
+      :cdr   => [:prim, lambda{ |list| cdr(list) }],
+      :list  => [:prim, lambda{ |*list| list(*list) }]
+    }
+
+    $global_env = [$list_env, $primitive_fun_env, $boolean_env]
   end
 end
--- calculater/list.rb
+++ calculater/list.rb
@@ -12,6 +12,22 @@ module Calculater
       list[1..-1]
     end
 
+    def null?(list)
+      list == []
+    end
+
+    def cons(a, b)
+      if not list?(b)
+        raise "sorry, we haven't implemented yet..."
+      else
+        [a] + b
+      end
+    end
+
+    def list(*list)
+      list
+    end
+
     def eval_list(exp, env)
       exp.map{ |e| _eval(e, env) }
     end
